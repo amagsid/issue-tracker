13453bcc945a84a1866523ea6530147c52ff8c5e
Ahmad <amagsid@gmail.com>
2024-01-19 21:22:19 +0100

    initial commit

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AddToCart from './Button';
+
+const ProductCard = () => {
+  return (
+    <div>
+      <AddToCart />
+    </div>
+  );
+};
+
+export default ProductCard;
