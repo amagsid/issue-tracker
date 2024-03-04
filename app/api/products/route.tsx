4d0f6f2b412ddfe0aa4c830e02c1688e24844c57
Ahmad <amagsid@gmail.com>
2024-03-04 09:41:12 +0100

    created pfoducts API

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -18,6 +18,7 @@ export async function POST(request: NextRequest) {
   const body = await request.json();
 
   const validation = schema.safeParse(body);
-  if (!validation.success) return NextResponse.json(validation.error.errors);
-  return NextResponse.json(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
+  return NextResponse.json({ id: 10, name: body.name, price: body.price });
 }
--- app/api/products/schema.tsx
+++ app/api/products/schema.tsx
@@ -1,7 +1,6 @@
 import { z } from 'zod';
 
 export const schema = z.object({
-  id: z.number().min(1),
   name: z.string().min(3),
   price: z.number().min(1),
 });
